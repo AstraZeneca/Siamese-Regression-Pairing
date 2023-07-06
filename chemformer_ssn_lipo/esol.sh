dab3ec7261e0f6e6c4aaf1271aa616df19c7b2b1
Zhang Ym <69457363+yumengzhang1998@users.noreply.github.com>
2023-07-06 22:35:41 +0200

    Delete chemformer_ssn_lipo directory

--- chemformer_ssn_lipo/esol.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python finetuneRegr_k_fold.py --name delaney --study_name delaney --data_path delaney/ --drp 0.0
-
-python finetuneRegr_k_fold.py --name delaney --study_name delaney --data_path delaney/ --drp 0.05
-
-python finetuneRegr_k_fold.py --name delaney --study_name delaney --data_path delaney/ --drp 0.1
-
-python finetuneRegr_k_fold.py --name delaney --study_name delaney --data_path delaney/ --drp 0.17
